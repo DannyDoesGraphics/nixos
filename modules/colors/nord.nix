fc65b01cb0072133988988edfe57d95fa86151d5
Danny <git@dannydoes.graphics>
2025-05-25 09:21:24 -0400

    Fixed ignores

---
+++ modules/colors/nord.nix
@@ -0,0 +1,36 @@
+# /etc/nixos/modules/colors/nord.nix
+
+{ lib, config, pkgs, ... }:
+
+let
+  nordPalette = {
+    color0  = "#2e3440";
+    color1  = "#3b4252";
+    color2  = "#434c5e";
+    color3  = "#4c566a";
+    color4  = "#d8dee9";
+    color5  = "#e5e9f0";
+    color6  = "#eceff4";
+    color7  = "#8fbcbb";
+    color8  = "#88c0d0";
+    color9  = "#81a1c1";
+    color10 = "#5e81ac";
+    color11 = "#bf616a";
+    color12 = "#d08770";
+    color13 = "#ebcb8b";
+    color14 = "#a3be8c";
+    color15 = "#b48ead";
+  };
+in {
+  options.ui.colors.palette = lib.mkOption {
+    type = lib.types.attrsOf lib.types.str;
+    default = nordPalette;
+    description = ''
+      A flat, indexed color palette (keys color0 … color15) used for UI theming.
+      Override this attrset to swap in any other color palette.
+    '';
+  };
+
+  config = { };
+}
+
---
+++ modules/nixos/user.nix
@@ -0,0 +1,42 @@
+# /etc/nixos/modules/user.nix
+
+{ lib, pkgs, config, ... }:
+{
+  options = {
+    user = {
+      enable = lib.mkEnableOption "This user";
+      full_name = lib.mkOption {
+        type = lib.types.str;
+        description = "This user's full name";
+      };
+      namespace = lib.mkOption {
+        type = lib.types.str;
+        description = "A unique namespace for the user";
+      };
+      extra_groups = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = ["wheel" "networkmanager" "audio"];
+        description = "Extra groups to add to the user";
+      };
+      home_file = lib.mkOption {
+        type = lib.types.path;
+        description = "Path to home.nix file";
+      };
+      shell = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.zsh;
+        description = "The user's default login shell";
+      };
+    };
+  };
+
+  config = lib.mkIf config.user.enable {
+    users.users.${config.user.namespace} = {
+      isNormalUser = true;
+      description = config.user.full_name;
+      extraGroups = config.user.extra_groups;
+      shell = config.user.shell;
+    };
+    home-manager.users.${config.user.namespace} = import config.user.home_file;
+  };
+}
