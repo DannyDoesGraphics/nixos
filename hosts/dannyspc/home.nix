16b63822e4c9989b4cf73f477c6afec411ed8023
Danny <git@dannydoes.graphics>
2025-05-26 15:30:57 -0400

    Update

--- hosts/dannyspc/home.nix
+++ hosts/dannyspc/home.nix
@@ -296,10 +296,10 @@ in {
     enable = true;
     defaultApplications = {
       "text/html" = "firefox.desktop";
-      "x-scheme-handler/http" = "librewolf.desktop";
-      "x-scheme-handler/https" = "librewolf.desktop";
-      "x-scheme-handler/about" = "librewolf.desktop";
-      "x-scheme-handler/unknown" = "librewolf.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
     };
   };
 
