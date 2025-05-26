56991e87cb79acbb6d65ff634d5e720671c16740
Danny <git@dannydoes.graphics>
2025-05-26 17:38:02 -0400

    Update

--- modules/hardware/gpus/nvidia.nix
+++ modules/hardware/gpus/nvidia.nix
@@ -7,17 +7,19 @@
     default = false;
     description = "Enable X11 server and set NVIDIA as the video driver";
   };
-  config =  {
-    services.xserver = lib.mkIf config.nvidia.xserver {
+  config = lib.mkMerge [
+    (lib.mkIf config.nvidia.xserver {
       enable = true;
       videoDrivers = [ "nvidia" ];
-    };
-    nixpkgs.config.nvidia.acceptLicense = true;
-     hardware.nvidia = {
-       modesetting.enable = true;
-       nvidiaSettings = true;
-       open = false; # Use proprietary drivers
-       package = config.boot.kernelPackages.nvidiaPackages.production;
-    };
-  };
+    })
+    {
+      nixpkgs.config.nvidia.acceptLicense = true;
+      hardware.nvidia = {
+        modesetting.enable = true;
+        nvidiaSettings = true;
+        open = false; # Use proprietary drivers
+        package = config.boot.kernelPackages.nvidiaPackages.production;
+      };
+    }
+  ];
 }
