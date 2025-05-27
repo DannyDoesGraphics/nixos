67b8e941420c253f12e8c01127baa5512d17966f
Danny <git@dannydoes.graphics>
2025-05-26 21:36:14 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
       url = "github:oxalica/nil";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen_browser = {
+    zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
