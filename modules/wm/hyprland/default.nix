e16988141b4fc705fb77ab314638ac59e4d0a083
Danny <git@dannydoes.graphics>
2025-05-26 20:49:46 -0400

    Update

--- modules/wm/hyprland/default.nix
+++ modules/wm/hyprland/default.nix
@@ -7,7 +7,7 @@
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     xwayland.enable = true;
-    sessionCommands = '''';
+    withUwsm = true;
   };
   services.displayManager = {
     sddm.enable = true;
