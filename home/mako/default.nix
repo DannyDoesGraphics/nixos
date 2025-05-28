d2219eb3e4a004171d93e4d3c782af5db9f2abbf
Danny <git@dannydoes.graphics>
2025-05-28 17:46:39 -0400

    Update

--- home/mako/default.nix
+++ home/mako/default.nix
@@ -4,6 +4,7 @@
     home.packages = [ pkgs.mako pkgs.libnotify ];
     services.mako = { enable = true; };
     home.file.".config/mako/config".text = ''
+      font = JetbrainsMono Nerd Font Mono Normal 12
       background-color = ${config.ui.colors.palette.color0}
       border-color = ${config.ui.colors.palette.color1}
     '';
