844bf40cfa3f74a50cc57484e4c5e2e0fd52f521
Danny <git@dannydoes.graphics>
2025-05-28 17:52:58 -0400

    Update

--- home/mako/default.nix
+++ home/mako/default.nix
@@ -4,8 +4,8 @@
     home.packages = [ pkgs.mako pkgs.libnotify ];
     services.mako = { enable = true; };
     home.file.".config/mako/config".text = ''
-      background-color = ${config.ui.colors.palette.color0}
-      border-color = ${config.ui.colors.palette.color1}
+      background-color = #FFFFFF
+      border-color = #FFFFFF
     '';
   };
 }
