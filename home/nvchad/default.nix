dd9dbc6cc69126afd50a8b64d465c87bc91b3d8a
Danny <git@dannydoes.graphics>
2025-05-28 20:35:41 -0400

    Update

--- home/nvchad/default.nix
+++ home/nvchad/default.nix
@@ -1,6 +1,7 @@
 # /etc/nixos/home/nvchad/default.nix
 { lib, config, pkgs, inputs, ... }: {
   config = {
+    imports = [ inputs.nvchad4nix.homeManagerModule ];
 
     programs.nvchad = {
       enable = true;
