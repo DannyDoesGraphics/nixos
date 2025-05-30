b7d375a08fc0052aa1ee5312e7410fd2486be005
Danny <git@dannydoes.graphics>
2025-05-30 14:30:38 -0400

    Update

--- home/steam/default.nix
+++ home/steam/default.nix
@@ -1,20 +1,20 @@
 # /etc/nixos/home/steam/default.nix
 { config, lib, pkgs, ... }: {
   config = {
-    home.packages = with pkgs;
-      [
-        # Steam with all necessary components
-        protonup
-        # HDR and gaming tools
-        vulkan-tools
-        vulkan-loader
-        mesa-demos
-      ];
+    home.packages = with pkgs; [
+      # Steam with all necessary components
+      protonup
+      lutris
+      # HDR and gaming tools
+      vulkan-tools
+      vulkan-loader
+      mesa-demos
+    ];
 
     # Configure Steam environment variables
     home.sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-        "${config.home.homeDirectory}/.steam/root/compatibilitytools.d";
+        "\\\${config.home.homeDirectory}/.steam/root/compatibilitytools.d";
       # HDR Support
       ENABLE_HDR_WSI = "1";
       DXVK_HDR = "1";
