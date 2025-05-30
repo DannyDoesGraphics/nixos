ad18b5d60b9f2ac347f969cab1e3d3ae9121ca70
Danny <git@dannydoes.graphics>
2025-05-30 03:03:29 -0400

    Update

--- modules/hardware/gpu/nvidia.nix
+++ modules/hardware/gpu/nvidia.nix
@@ -6,41 +6,37 @@
     default = false;
     description = "Enable X11 server and set NVIDIA as the video driver";
   };
-  config = lib.mkMerge [
-    (lib.mkIf config.nvidia.xserver {
-      services.xserver = {
-        enable = true;
-        videoDrivers = [ "nvidia" ];
-      };
-    })
-    {
-      nixpkgs.config.nvidia.acceptLicense = true;
-      hardware.nvidia = {
-        modesetting.enable = true;
-        nvidiaSettings = true;
-        open = false; # Use proprietary drivers
-        package = config.boot.kernelPackages.nvidiaPackages.production;
-        # HDR Support
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
-      };
-      nixpkgs.config.cudaSupport = true;
-      environment.systemPackages = with pkgs; [
-        cudaPackages.cudatoolkit
-        cudaPackages.cudnn
-        cudaPackages.cuda_cudart
+  config = {
+    services.xserver = {
+      enable = true;
+      videoDrivers = [ "nvidia" ];
+    };
+    nixpkgs.config.nvidia.acceptLicense = true;
+    hardware.nvidia = {
+      modesetting.enable = true;
+      nvidiaSettings = true;
+      open = false; # Use proprietary drivers
+      package = config.boot.kernelPackages.nvidiaPackages.production;
+      # HDR Support
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+    };
+    nixpkgs.config.cudaSupport = true;
+    environment.systemPackages = with pkgs; [
+      cudaPackages.cudatoolkit
+      cudaPackages.cudnn
+      cudaPackages.cuda_cudart
+    ];
+    environment.sessionVariables = {
+      CUDA_HOME = "${pkgs.cudaPackages.cudatoolkit}";
+      LD_LIBRARY_PATH = lib.makeLibraryPath [
+        "${pkgs.cudaPackages.cudatoolkit}"
+        "${pkgs.cudaPackages.cudatoolkit}/lib64"
+        pkgs.cudaPackages.cudnn
+        pkgs.cudaPackages.cuda_cudart
+        pkgs.stdenv.cc.cc.lib
       ];
-      environment.sessionVariables = {
-        CUDA_HOME = "${pkgs.cudaPackages.cudatoolkit}";
-        LD_LIBRARY_PATH = lib.makeLibraryPath [
-          "${pkgs.cudaPackages.cudatoolkit}"
-          "${pkgs.cudaPackages.cudatoolkit}/lib64"
-          pkgs.cudaPackages.cudnn
-          pkgs.cudaPackages.cuda_cudart
-          pkgs.stdenv.cc.cc.lib
-        ];
-        CUDA_MODULE_LOADING = "LAZY";
-      };
-    }
-  ];
+      CUDA_MODULE_LOADING = "LAZY";
+    };
+  };
 }
